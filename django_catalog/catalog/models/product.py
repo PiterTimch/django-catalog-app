ea85653db75db16cae70e061d2a065f74fe6ffd8
Petro <petrotymchuck@gmail.com>
2026-08-20 08:25:44 +0300

    Refactor Product model to use ImageField for image uploads and add utility function for image path generation

--- django_catalog/catalog/models/product.py
+++ django_catalog/catalog/models/product.py
@@ -1,5 +1,6 @@
 from django.db import models
 from .category import Category
+from catalog.utils import product_image_path
 
 
 class Product(models.Model):
@@ -8,7 +9,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.CharField(max_length=500, blank=True)
+    image = models.ImageField(upload_to=product_image_path, blank=True, null=True)
 
     def __str__(self):
         return self.name
